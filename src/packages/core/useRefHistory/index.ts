0471e2271701c3132fd0ea631a1b6fb9bb7e6e43
pei-pay <gleektennis@icloud.com>
2024-03-31 23:08:39 +0900

    feat: useRefHistory

--- src/packages/core/useRefHistory/index.ts
+++ src/packages/core/useRefHistory/index.ts
@@ -1,15 +1,37 @@
 import { type Ref } from "vue";
+import { watchIgnorable } from "@/packages/shared";
+
 import { useManualRefHistory, type UseManualRefHistoryReturn } from "../useManualRefHistory";
 
 export interface UseRefHistoryReturn<Raw, Serialized> extends UseManualRefHistoryReturn<Raw, Serialized> {
 }
 
-// TODO: Serialized って何?
+// TODO: Serialized は最低限だと必要ないかも
 export function useRefHistory<Raw, Serialized = Raw>(
   source: Ref<Raw>
 ) {
 
-  const manualHistory = useManualRefHistory(source);
+  const {
+    ignoreUpdates,
+  } = watchIgnorable(
+    source,
+    commit,
+  );
+
+
+
+  function setSource(source: Ref<Raw>, value: Raw) {
+    ignoreUpdates(() => {
+      source.value = value;
+    });
+  }
+
+  const manualHistory = useManualRefHistory(source, { setSource });
+  const { commit: manualCommit } = manualHistory;
+
+  function commit() {
+    manualCommit();
+  }
 
   return {
     ...manualHistory
