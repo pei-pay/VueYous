285bb6fffbb246913e3f159201a2e9e7e5e5ca39
artsmp <artsmp@126.com>
2024-04-16 18:03:15 +0800

    chore: lint

--- packages/.vitepress/config/index.ts
+++ packages/.vitepress/config/index.ts
@@ -1,21 +1,21 @@
-import type { DefaultTheme, UserConfig } from "vitepress";
-import { defineConfig } from "vitepress";
-import { sharedConfig } from "./shared";
-import { ja } from "./ja";
-import { en } from "./en";
-import { zh } from "./zh";
+import type { DefaultTheme, UserConfig } from 'vitepress'
+import { defineConfig } from 'vitepress'
+import { sharedConfig } from './shared'
+import { ja } from './ja'
+import { en } from './en'
+import { zh } from './zh'
 
 export type i18nTheme = Partial<
-  NonNullable<UserConfig<DefaultTheme.Config>["themeConfig"]>
->;
+  NonNullable<UserConfig<DefaultTheme.Config>['themeConfig']>
+>
 
 export default defineConfig({
   ...sharedConfig,
   themeConfig: sharedConfig.themeConfig,
   locales: {
-    root: { label: "English", lang: "en", themeConfig: { ...en } },
-    ja: { label: "日本語", lang: "ja", themeConfig: { ...ja } },
-    zh: { label: "简体中文", lang: " zh", themeConfig: { ...zh } },
+    root: { label: 'English', lang: 'en', themeConfig: { ...en } },
+    ja: { label: '日本語', lang: 'ja', themeConfig: { ...ja } },
+    zh: { label: '简体中文', lang: ' zh', themeConfig: { ...zh } },
     // other languages...
   },
-});
+})
