462c4ee01f24e6b02ade8c280433a7c18e297767
pei-pay <gleektennis@icloud.com>
2024-04-28 13:18:00 +0900

    chapter: add starter

--- packages/.vitepress/config/ja.ts
+++ packages/.vitepress/config/ja.ts
@@ -3,12 +3,20 @@ import type { i18nTheme } from '.'
 const nav: i18nTheme['nav'] = [
   { text: 'Home', link: '/ja/' },
   { text: 'Guide', link: '/ja/guide/' },
+  { text: 'Starter', link: '/ja/starter/' },
 ]
 
 const sidebar: i18nTheme['sidebar'] = [
-  { text: 'はじめに', link: '/ja/guide/' },
-  { text: 'VueUseとは', link: '/ja/guide/what-is-vueuse' },
-  { text: '環境構築', link: '/ja/guide/setup' },
+  { text: 'Guide', items: [
+    { text: 'はじめに', link: '/ja/guide/' },
+    { text: 'VueUseとは', link: '/ja/guide/what-is-vueuse' },
+    { text: '環境構築', link: '/ja/guide/setup' },
+  ] },
+  { text: 'Starter', items: [
+    { text: 'What is Starter', link: '/ja/starter/' },
+    { text: 'useRefHistory', link: '/ja/starter/useRefHistory' },
+  ] },
+
 ]
 
 export const ja: i18nTheme = { nav, sidebar }
